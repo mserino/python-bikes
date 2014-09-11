f5079d5e965e16a17f57ef47fb04fdc1ac54c89a
mserino <m.serino@hotmail.it>
2014-09-11 17:23:20 +0100

    Person cannot have more than one bike

--- person.py
+++ person.py
@@ -2,6 +2,19 @@ class Person(object):
 
 	def __init__(self):
 		self.bikes = []
+		self.capacity = 1
 
 	def dock(self, bike):
-		self.bikes.append(bike)
+		if len(self.bikes) < self.capacity:
+			self.bikes.append(bike)
+		else:
+			return "Cannot dock more than one bike"
+
+	def release(self, bike):
+		self.bikes.remove(bike)
+
+	def is_full(self):
+		if self.capacity == len(self.bikes):
+			return "full"
+		else:
+			return "not full"
--- person_test.py
+++ person_test.py
@@ -7,15 +7,35 @@ class PersonTest(unittest.TestCase):
 	def setUp(self):
 		self.person = Person()
 		self.bike = Bike()
-
+		self.bike2 = Bike()
+	
 	def test_person_has_no_bikes(self):
-		for bike in self.person.bikes:
-			self.assertEqual(self.person.bikes.count(bike), 0)
+		self.assertEqual(len(self.person.bikes), 0)
 
 	def test_person_can_have_a_bike(self):
 		self.person.dock(self.bike)
-		for bike in self.person.bikes:
-			self.assertEqual(self.person.bikes.count(bike), 1)
+		self.assertEqual(len(self.person.bikes), 1)
+		self.assertIn(self.bike, self.person.bikes)
+
+	def test_person_can_release_a_bike(self):
+		self.person.dock(self.bike)
+		self.person.release(self.bike)
+		self.assertEqual(len(self.person.bikes), 0)
+
+	def test_person_can_have_only_one_bike(self):
+		self.assertEqual(self.person.capacity, 1)
+
+	def test_person_is_not_full(self):
+		self.assertEqual(self.person.is_full(), "not full")
+
+	def test_person_is_full(self):
+		self.person.dock(self.bike)
+		self.assertEqual(self.person.is_full(), "full")
+
+	def test_person_cannot_have_another_bike_if_full(self):
+		self.person.dock(self.bike)
+		self.person.dock(self.bike2)
+		self.assertEqual(self.person.dock(self.bike2), "Cannot dock more than one bike")
 
 if __name__ == '__main__':
 	unittest.main()
